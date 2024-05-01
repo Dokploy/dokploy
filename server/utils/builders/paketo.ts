ad58cce2cda402cdc0380251ab1162ab823a11fb
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2024-05-01 17:42:52 -0600

    fix: change for in to for of to access to real value instead of the index

--- server/utils/builders/paketo.ts
+++ server/utils/builders/paketo.ts
@@ -6,33 +6,33 @@ import { prepareEnvironmentVariables } from "../docker/utils";
 
 // TODO: integrate in the vps sudo chown -R $(whoami) ~/.docker
 export const buildPaketo = async (
-  application: ApplicationNested,
-  writeStream: WriteStream,
+	application: ApplicationNested,
+	writeStream: WriteStream,
 ) => {
-  const { env, appName } = application;
-  const buildAppDirectory = getBuildAppDirectory(application);
-  const envVariables = prepareEnvironmentVariables(env);
-  try {
-    const args = [
-      "build",
-      appName,
-      "--path",
-      buildAppDirectory,
-      "--builder",
-      "paketobuildpacks/builder-jammy-full",
-    ];
+	const { env, appName } = application;
+	const buildAppDirectory = getBuildAppDirectory(application);
+	const envVariables = prepareEnvironmentVariables(env);
+	try {
+		const args = [
+			"build",
+			appName,
+			"--path",
+			buildAppDirectory,
+			"--builder",
+			"paketobuildpacks/builder-jammy-full",
+		];
 
-    for (const env in envVariables) {
-      args.push("--env", env);
-    }
+		for (const env of envVariables) {
+			args.push("--env", env);
+		}
 
-    await spawnAsync("pack", args, (data) => {
-      if (writeStream.writable) {
-        writeStream.write(data);
-      }
-    });
-    return true;
-  } catch (e) {
-    throw e;
-  }
+		await spawnAsync("pack", args, (data) => {
+			if (writeStream.writable) {
+				writeStream.write(data);
+			}
+		});
+		return true;
+	} catch (e) {
+		throw e;
+	}
 };
