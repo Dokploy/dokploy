63b5fdaa7165d00301231823205b82a625df4f51
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2024-05-01 19:44:41 -0600

    feat: add canary builds for testing purposes

--- docker/push.sh
+++ docker/push.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-VERSION=$(node -p "require('./package.json').version")
+# Determine the type of build based on the first script argument
+BUILD_TYPE=${1:-production}
 
-docker push "dokploy/dokploy:${VERSION}"
-docker push "dokploy/dokploy:latest"
+if [ "$BUILD_TYPE" == "canary" ]; then
+    TAG="canary"
+    echo PUSHING CANARY
+    docker push "dokploy/dokploy:${TAG}"
+else
+    echo  "PUSHING PRODUCTION"
+    VERSION=$(node -p "require('./package.json').version")
+    docker push "dokploy/dokploy:${VERSION}"
+    docker push "dokploy/dokploy:latest"
+fi
