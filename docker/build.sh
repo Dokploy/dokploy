59bd46908f4cf169f30a17a72f91ed7e9f673895
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2024-05-04 02:19:35 -0600

    chore: remove --push flag

--- docker/build.sh
+++ docker/build.sh
@@ -10,16 +10,8 @@ else
     TAG="$VERSION"
 fi
 
-# Ensure Docker's buildx plugin is being used and set as the default builder
 BUILDER=$(docker buildx create --use)
 
-# Build and push the images for different architectures
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 --pull --rm -t "dokploy/dokploy:${TAG}" -f 'Dockerfile' --push .
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 --pull --rm -t "dokploy/dokploy:${TAG}" -f 'Dockerfile' .
 
-if [ "$BUILD_TYPE" != "canary" ]; then
-    # Tag the production build as latest for all architectures and push
-    docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 --pull --rm -t "dokploy/dokploy:latest" -f 'Dockerfile' --push .
-fi
-
-# Clean up the buildx builder instance
 docker buildx rm $BUILDER
