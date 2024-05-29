9d068047d425efe56335231071cb0f4d7f1192ef
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2024-05-29 00:12:51 -0600

    feat: add vitest to ci/cd

--- __test__/vitest.config.ts
+++ __test__/vitest.config.ts
@@ -4,10 +4,12 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
 	plugins: [
 		tsconfigPaths({
-			root: "../",
+			root: "./",
+			projects: ["tsconfig.json"],
 		}),
 	],
 	test: {
-		include: ["**/*.test.ts"],
+		include: ["__test__/**/*.test.ts"], // Incluir solo los archivos de test en el directorio __test__
+		exclude: ["**/node_modules/**", "**/dist/**", "**/.docker/**"],
 	},
 });
