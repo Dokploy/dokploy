5038123e89a344f5744a5e2e2f40f1b50a2da1cd
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2024-05-29 00:13:49 -0600

    revert: back to initial version

--- docker/push.sh
+++ docker/push.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
 # Determine the type of build based on the first script argument
-# BUILD_TYPE=${1:-production}
+BUILD_TYPE=${1:-production}
 
-# BUILDER=$(docker buildx create --use)
+BUILDER=$(docker buildx create --use)
 
+if [ "$BUILD_TYPE" == "canary" ]; then
+    TAG="canary"
+    echo PUSHING CANARY
+        docker buildx build --platform linux/amd64,linux/arm64 --pull --rm -t "dokploy/dokploy:${TAG}" -f 'Dockerfile' --push .
+else
+    echo  "PUSHING PRODUCTION"
+    VERSION=$(node -p "require('./package.json').version")
+    docker buildx build --platform linux/amd64,linux/arm64 --pull --rm -t "dokploy/dokploy:latest" -t "dokploy/dokploy:${VERSION}" -f 'Dockerfile' --push .
+fi
 
-# if [ "$BUILD_TYPE" == "canary" ]; then
-#     TAG="canary"
-#     echo PUSHING CANARY
-#     docker  build --platform linux/amd64 --pull --rm -t "dokploy/dokploy:${TAG}" -f 'Dockerfile' .
-# else
-#     echo  "PUSHING PRODUCTION"
-#     VERSION=$(node -p "require('./package.json').version")
-#     docker buildx build --platform linux/amd64,linux/arm64 --pull --rm -t "dokploy/dokploy:latest" -t "dokploy/dokploy:${VERSION}" -f 'Dockerfile' --push .
-# fi
-
-docker  build --platform linux/amd64 --pull --rm -t "dokploy/dokploy:feature" -f 'Dockerfile' .
-
-# docker buildx rm $BUILDER
+docker buildx rm $BUILDER
 
