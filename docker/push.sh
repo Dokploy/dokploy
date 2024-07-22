104177a311a2f4a99e22a0043d7a2342b1561efb
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2024-07-22 00:57:55 -0600

    refactor: script

--- docker/push.sh
+++ docker/push.sh
@@ -3,16 +3,18 @@
 # Determine the type of build based on the first script argument
 BUILD_TYPE=${1:-production}
 
-BUILDER=$(docker buildx create --use)
+
+BUILDER_NAME="dokploy-builder"
+docker buildx create --name $BUILDER_NAME --use
 
 if [ "$BUILD_TYPE" == "canary" ]; then
     TAG="canary"
     echo PUSHING CANARY
-        docker buildx build --platform linux/amd64,linux/arm64 --pull --rm -t "dokploy/dokploy:${TAG}" -f 'Dockerfile' --push .
+        docker buildx build --builder $BUILDER_NAME --platform linux/amd64,linux/arm64 --pull --rm -t "dokploy/dokploy:${TAG}" -f 'Dockerfile' --push .
 else
     echo  "PUSHING PRODUCTION"
     VERSION=$(node -p "require('./package.json').version")
-    docker buildx build --platform linux/amd64,linux/arm64 --pull --rm -t "dokploy/dokploy:latest" -t "dokploy/dokploy:${VERSION}" -f 'Dockerfile' --push .
+    docker buildx build --builder $BUILDER_NAME --platform linux/amd64,linux/arm64 --pull --rm -t "dokploy/dokploy:latest" -t "dokploy/dokploy:${VERSION}" -f 'Dockerfile' --push .
 fi
 
 docker buildx rm $BUILDER
