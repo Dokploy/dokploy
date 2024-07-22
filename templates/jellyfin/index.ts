54aaa511d5672b1f742a89f95b495fb5e8d9fba2
Samuel <samuel.barraud1@gmail.com>
2024-07-22 09:20:16 +0300

    feat: add jellyfin template

--- templates/jellyfin/index.ts
+++ templates/jellyfin/index.ts
@@ -7,10 +7,13 @@ import {
 } from "../utils";
 
 export function generate(schema: Schema): Template {
+  const mainServiceHash = generateHash(schema.projectName);
   const randomDomain = generateRandomDomain(schema);
-
+  const port = 8096;
   const envs = [
     `JELLYFIN_HOST=${randomDomain}`,
+    `HASH=${mainServiceHash}`,
+    `JELLYFIN_PORT=${port}`
   ];
 
   return {
