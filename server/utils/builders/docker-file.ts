6d71eac2219efb92ce9482ed69d89740976e425d
Lorenzo Migliorero <lorenzo.migliorero@wetransfer.com>
2024-07-22 21:42:07 +0200

    revert: env file

--- server/utils/builders/docker-file.ts
+++ server/utils/builders/docker-file.ts
@@ -4,18 +4,21 @@ import { prepareBuildArgs } from "@/server/utils/docker/utils";
 import * as tar from "tar-fs";
 import type { ApplicationNested } from ".";
 import { getBuildAppDirectory } from "../filesystem/directory";
+import { createEnvFile } from "./utils";
 
 export const buildCustomDocker = async (
 	application: ApplicationNested,
 	writeStream: WriteStream,
 ) => {
-	const { appName, buildArgs } = application;
+	const { appName, env, buildArgs } = application;
 	const dockerFilePath = getBuildAppDirectory(application);
 	try {
 		const image = `${appName}`;
 		const contextPath =
 			dockerFilePath.substring(0, dockerFilePath.lastIndexOf("/") + 1) || ".";
 		const tarStream = tar.pack(contextPath);
+
+		createEnvFile(dockerFilePath, env);
 
 		const stream = await docker.buildImage(tarStream, {
 			t: image,
