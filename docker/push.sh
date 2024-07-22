56731a4269c91d16c31efded95ab5a188bf51d5e
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2024-07-22 01:11:07 -0600

    refactor: update

--- docker/push.sh
+++ docker/push.sh
@@ -10,11 +10,11 @@ docker buildx create --name $BUILDER_NAME --use
 if [ "$BUILD_TYPE" == "canary" ]; then
     TAG="canary"
     echo PUSHING CANARY
-        docker buildx build --builder $BUILDER_NAME --platform linux/amd64,linux/arm64 --pull --rm -t "dokploy/dokploy:${TAG}" -f 'Dockerfile' --push .
+        docker buildx build --builder $BUILDER_NAME --platform linux/amd64,linux/arm64  -t "dokploy/dokploy:${TAG}" -f 'Dockerfile' --push .
 else
     echo  "PUSHING PRODUCTION"
     VERSION=$(node -p "require('./package.json').version")
-    docker buildx build --builder $BUILDER_NAME --platform linux/amd64,linux/arm64 --pull --rm -t "dokploy/dokploy:latest" -t "dokploy/dokploy:${VERSION}" -f 'Dockerfile' --push .
+    docker buildx build --builder $BUILDER_NAME --platform linux/amd64,linux/arm64  -t "dokploy/dokploy:latest" -t "dokploy/dokploy:${VERSION}" -f 'Dockerfile' --push .
 fi
 
 docker buildx rm $BUILDER
