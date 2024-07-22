c13eb65b5a424968d3a4b3960278fab8f28ebb72
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2024-07-22 12:58:14 -0600

    refactor: format

--- templates/jellyfin/index.ts
+++ templates/jellyfin/index.ts
@@ -1,22 +1,22 @@
 // EXAMPLE
 import {
-  generateHash,
-  generateRandomDomain,
-  type Template,
-  type Schema,
+	type Schema,
+	type Template,
+	generateHash,
+	generateRandomDomain,
 } from "../utils";
 
 export function generate(schema: Schema): Template {
-  const mainServiceHash = generateHash(schema.projectName);
-  const randomDomain = generateRandomDomain(schema);
-  const port = 8096;
-  const envs = [
-    `JELLYFIN_HOST=${randomDomain}`,
-    `HASH=${mainServiceHash}`,
-    `JELLYFIN_PORT=${port}`
-  ];
+	const mainServiceHash = generateHash(schema.projectName);
+	const randomDomain = generateRandomDomain(schema);
+	const port = 8096;
+	const envs = [
+		`JELLYFIN_HOST=${randomDomain}`,
+		`HASH=${mainServiceHash}`,
+		`JELLYFIN_PORT=${port}`,
+	];
 
-  return {
-    envs,
-  };
+	return {
+		envs,
+	};
 }
