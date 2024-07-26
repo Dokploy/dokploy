7e39be4ca16e01f5476d8911214fa3951f174e39
Lorenzo Migliorero <lorenzo.migliorero@wetransfer.com>
2024-07-26 09:51:35 +0200

    fix: migrations

--- drizzle/0027_fantastic_squadron_sinister.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE "compose" ADD COLUMN "customGitSSHKeyId" text;--> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "compose" ADD CONSTRAINT "compose_customGitSSHKeyId_ssh-key_sshKeyId_fk" FOREIGN KEY ("customGitSSHKeyId") REFERENCES "public"."ssh-key"("sshKeyId") ON DELETE set null ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
---> statement-breakpoint
-ALTER TABLE "compose" DROP COLUMN IF EXISTS "customGitSSHKey";
