f0acc980cd1b4002f5429e760b278eac19bdcad3
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2024-07-27 22:50:48 -0600

    refactor: uncomment

--- apps/dokploy/next.config.mjs
+++ apps/dokploy/next.config.mjs
@@ -12,12 +12,12 @@ const __dirname = path.dirname(__filename);
 /** @type {import("next").NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	eslint: {
-		ignoreDuringBuilds: true,
-	},
-	typescript: {
-		ignoreBuildErrors: true,
-	},
+	// eslint: {
+	// 	ignoreDuringBuilds: true,
+	// },
+	// typescript: {
+	// 	ignoreBuildErrors: true,
+	// },
 	webpack: (config) => {
 		config.plugins.push(
 			new CopyWebpackPlugin({
