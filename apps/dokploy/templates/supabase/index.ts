3a8b2867b690d6bd9223fb9d4f48872bcd18354a
songtianlun <tianlun.song@foxmail.com>
2024-08-18 13:02:25 +0800

    fix: supabase add network

--- apps/dokploy/templates/supabase/index.ts
+++ apps/dokploy/templates/supabase/index.ts
@@ -19,6 +19,7 @@ export function generate(schema: Schema): Template {
 	const envs = [
 		`POSTGRES_PASSWORD=${postgresPassword}`,
         `JWT_SECRET=${jwtSecret}`, 
+		`BASE_URL=http://${randomDomain}`,
         `ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE`,
         `SERVICE_ROLE_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJzZXJ2aWNlX3JvbGUiLAogICAgImlzcyI6ICJzdXBhYmFzZS1kZW1vIiwKICAgICJpYXQiOiAxNjQxNzY5MjAwLAogICAgImV4cCI6IDE3OTk1MzU2MDAKfQ.DaYlNEoUrrEn2Ig7tqibS-PHK5vgusbcbo7X36XVt4Q`,
         `DASHBOARD_USERNAME=supabase`,
