4d2354df47d39d539e3808e5a4babdee1300a097
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2024-08-29 22:05:16 -0600

    Update index.ts

--- apps/dokploy/templates/typebot/index.ts
+++ apps/dokploy/templates/typebot/index.ts
@@ -29,10 +29,10 @@ export function generate(schema: Schema): Template {
 		`NEXTAUTH_URL=http://${builderDomain}`,
 		`NEXT_PUBLIC_VIEWER_URL=http://${viewerDomain}`,
 		"ADMIN_EMAIL=typebot@example.com",
-		"SMTP_HOST=",
+		"SMTP_HOST='Fill'",
 		"SMTP_PORT=25",
-		"SMTP_USERNAME=",
-		"SMTP_PASSWORD=",
+		"SMTP_USERNAME='Fill'",
+		"SMTP_PASSWORD='Fill'",
 		"NEXT_PUBLIC_SMTP_FROM=typebot@example.com",
 		"DEFAULT_WORKSPACE_PLAN=UNLIMITED",
 	];
