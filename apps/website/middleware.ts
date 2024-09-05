d3108ebf65bd514c63dd05a8ebcdbb931521ee49
JiPai <github@jipai.moe>
2024-09-05 20:19:10 +0800

    feat: let not-found page work with locale

--- apps/website/middleware.ts
+++ apps/website/middleware.ts
@@ -1,9 +1,9 @@
-import createMiddleware from 'next-intl/middleware';
-import {routing} from './i18n/routing';
- 
-export default createMiddleware(routing);
- 
+import createMiddleware from 'next-intl/middleware'
+import { routing } from './i18n/routing'
+
+export default createMiddleware(routing, { localeDetection: false })
+
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/', '/(zh-Hans|en)/:path*']
-};
+	// Match only internationalized pathnames
+	matcher: ['/((?!_next|.*\\..*).*)']
+}
