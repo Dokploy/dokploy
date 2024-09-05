363fb71804a678deeab8add31845fa5b4067b036
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2024-09-05 11:51:56 -0600

    Update index.js

--- apps/dokploy/i18n/InternationalizationKit/smartLocale/index.js
+++ apps/dokploy/i18n/InternationalizationKit/smartLocale/index.js
@@ -1,5 +1,5 @@
-import fs from 'fs';
-import path from 'path';
+import fs from 'node:fs';
+import path from 'node:path';
 
 /**
  * A Webpack plugin to generate type definitions and import statements
