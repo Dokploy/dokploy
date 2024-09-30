071a9d5104ee5f8e376fec0fb4687b3b0f9b10cf
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2024-09-29 20:35:25 -0600

    refactor: cleanup dependencies

--- packages/builders/esbuild.config.ts
+++ packages/builders/esbuild.config.ts
@@ -1,25 +1,25 @@
-import esbuild from "esbuild";
+// import esbuild from "esbuild";
 
-try {
-	esbuild
-		.build({
-			entryPoints: ["./src/**/*.ts"],
-			bundle: true,
-			platform: "node",
-			format: "cjs",
-			target: "node18",
-			outExtension: { ".js": ".js" },
-			minify: true,
-			outdir: "dist",
-			tsconfig: "tsconfig.server.json",
-			packages: "external",
-			alias: {
-				"@/server": "./src",
-			},
-		})
-		.catch(() => {
-			return process.exit(1);
-		});
-} catch (error) {
-	console.log(error);
-}
+// try {
+// 	esbuild
+// 		.build({
+// 			entryPoints: ["./src/**/*.ts"],
+// 			bundle: true,
+// 			platform: "node",
+// 			format: "cjs",
+// 			target: "node18",
+// 			outExtension: { ".js": ".js" },
+// 			minify: true,
+// 			outdir: "dist",
+// 			tsconfig: "tsconfig.server.json",
+// 			packages: "external",
+// 			alias: {
+// 				"@/server": "./src",
+// 			},
+// 		})
+// 		.catch(() => {
+// 			return process.exit(1);
+// 		});
+// } catch (error) {
+// 	console.log(error);
+// }
