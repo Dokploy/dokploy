1e6dbb5e8e8f8b69f97cc5731def086eb6074b3f
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2024-10-22 01:25:13 -0600

    feat(dokploy): add reset password for cloud

---
+++ apps/dokploy/drizzle/0044_wandering_butterfly.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "auth" ADD COLUMN "resetPasswordToken" text;--> statement-breakpoint
+ALTER TABLE "auth" ADD COLUMN "resetPasswordExpiresAt" text;
--- apps/dokploy/pages/send-reset-password.tsx
+++ apps/dokploy/pages/send-reset-password.tsx
@@ -1,5 +1,6 @@
 import { Login2FA } from "@/components/auth/login-2fa";
 import { OnboardingLayout } from "@/components/layouts/onboarding-layout";
+import { AlertBlock } from "@/components/shared/alert-block";
 import { Logo } from "@/components/shared/logo";
 import { Button } from "@/components/ui/button";
 import {
@@ -18,7 +19,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { api } from "@/utils/api";
-import { IS_CLOUD, isAdminPresent, validateRequest } from "@dokploy/server";
+import { IS_CLOUD } from "@dokploy/server";
 import { zodResolver } from "@hookform/resolvers/zod";
 import type { GetServerSidePropsContext } from "next";
 import Link from "next/link";
@@ -37,15 +38,6 @@ const loginSchema = z.object({
 		.email({
 			message: "Email must be a valid email",
 		}),
-
-	password: z
-		.string()
-		.min(1, {
-			message: "Password is required",
-		})
-		.min(8, {
-			message: "Password must be at least 8 characters",
-		}),
 });
 
 type Login = z.infer<typeof loginSchema>;
@@ -55,20 +47,17 @@ type AuthResponse = {
 	authId: string;
 };
 
-interface Props {
-	IS_CLOUD: boolean;
-}
-export default function Home({ IS_CLOUD }: Props) {
+export default function Home() {
 	const [temp, setTemp] = useState<AuthResponse>({
 		is2FAEnabled: false,
 		authId: "",
 	});
-	const { mutateAsync, isLoading } = api.auth.login.useMutation();
+	const { mutateAsync, isLoading, isError, error } =
+		api.auth.sendResetPasswordEmail.useMutation();
 	const router = useRouter();
 	const form = useForm<Login>({
 		defaultValues: {
 			email: "",
-			password: "",
 		},
 		resolver: zodResolver(loginSchema),
 	});
@@ -80,20 +69,14 @@ export default function Home({ IS_CLOUD }: Props) {
 	const onSubmit = async (values: Login) => {
 		await mutateAsync({
 			email: values.email,
-			password: values.password,
 		})
 			.then((data) => {
-				if (data.is2FAEnabled) {
-					setTemp(data);
-				} else {
-					toast.success("Signin succesfully", {
-						duration: 2000,
-					});
-					router.push("/dashboard/projects");
-				}
+				toast.success("Email sent", {
+					duration: 2000,
+				});
 			})
 			.catch(() => {
-				toast.error("Signin failed", {
+				toast.error("Error to send email", {
 					duration: 2000,
 				});
 			});
@@ -101,21 +84,23 @@ export default function Home({ IS_CLOUD }: Props) {
 	return (
 		<div className="flex  h-screen w-full items-center justify-center ">
 			<div className="flex flex-col items-center gap-4 w-full">
-				<Link
-					href="https://dokploy.com"
-					target="_blank"
-					className="flex flex-row items-center gap-2"
-				>
+				<Link href="/" className="flex flex-row items-center gap-2">
 					<Logo />
 					<span className="font-medium text-sm">Dokploy</span>
 				</Link>
-				<CardTitle className="text-2xl font-bold">Sign in</CardTitle>
+				<CardTitle className="text-2xl font-bold">Reset Password</CardTitle>
 				<CardDescription>
-					Enter your credentials to access your account
+					Enter your email to reset your password
 				</CardDescription>
+
 				<Card className="mx-auto w-full max-w-lg bg-transparent ">
 					<div className="p-3.5" />
 					<CardContent>
+						{isError && (
+							<AlertBlock type="error" className="my-2">
+								{error?.message}
+							</AlertBlock>
+						)}
 						{!temp.is2FAEnabled ? (
 							<Form {...form}>
 								<form
@@ -136,30 +121,13 @@ export default function Home({ IS_CLOUD }: Props) {
 												</FormItem>
 											)}
 										/>
-										<FormField
-											control={form.control}
-											name="password"
-											render={({ field }) => (
-												<FormItem>
-													<FormLabel>Password</FormLabel>
-													<FormControl>
-														<Input
-															type="password"
-															placeholder="Password"
-															{...field}
-														/>
-													</FormControl>
-													<FormMessage />
-												</FormItem>
-											)}
-										/>
 
 										<Button
 											type="submit"
 											isLoading={isLoading}
 											className="w-full"
 										>
-											Login
+											Send Reset Link
 										</Button>
 									</div>
 								</form>
@@ -172,32 +140,12 @@ export default function Home({ IS_CLOUD }: Props) {
 							<div className="mt-4 text-center text-sm flex flex-row justify-center gap-2">
 								<Link
 									className="hover:underline text-muted-foreground"
-									href="/register"
+									href="/"
 								>
-									Create an account
+									Login
 								</Link>
 							</div>
-
-							<div className="mt-4 text-sm flex flex-row justify-center gap-2">
-								{IS_CLOUD ? (
-									<Link
-										className="hover:underline text-muted-foreground"
-										href="/send-reset-password"
-									>
-										Lost your password?
-									</Link>
-								) : (
-									<Link
-										className="hover:underline text-muted-foreground"
-										href="https://docs.dokploy.com/docs/core/get-started/reset-password"
-										target="_blank"
-									>
-										Lost your password?
-									</Link>
-								)}
-							</div>
 						</div>
-						<div className="p-2" />
 					</CardContent>
 				</Card>
 			</div>
@@ -209,51 +157,16 @@ Home.getLayout = (page: ReactElement) => {
 	return <OnboardingLayout>{page}</OnboardingLayout>;
 };
 export async function getServerSideProps(context: GetServerSidePropsContext) {
-	if (IS_CLOUD) {
-		try {
-			const { user } = await validateRequest(context.req, context.res);
-
-			if (user) {
-				return {
-					redirect: {
-						permanent: true,
-						destination: "/dashboard/projects",
-					},
-				};
-			}
-		} catch (error) {}
-
-		return {
-			props: {
-				IS_CLOUD: IS_CLOUD,
-			},
-		};
-	}
-	const hasAdmin = await isAdminPresent();
-
-	if (!hasAdmin) {
+	if (!IS_CLOUD) {
 		return {
 			redirect: {
 				permanent: true,
-				destination: "/register",
-			},
-		};
-	}
-
-	const { user } = await validateRequest(context.req, context.res);
-
-	if (user) {
-		return {
-			redirect: {
-				permanent: true,
-				destination: "/dashboard/projects",
+				destination: "/",
 			},
 		};
 	}
 
 	return {
-		props: {
-			hasAdmin,
-		},
+		props: {},
 	};
 }
