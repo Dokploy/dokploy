6b7712e35fc2ac6ccc611dc242b3faf94d3cb29a
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2024-11-04 10:10:52 -0600

    fix(dokploy): filter notifications by admin

--- packages/server/src/utils/backups/mysql.ts
+++ packages/server/src/utils/backups/mysql.ts
@@ -46,6 +46,7 @@ export const runMySqlBackup = async (mysql: MySql, backup: BackupSchedule) => {
 			projectName: project.name,
 			databaseType: "mysql",
 			type: "success",
+			adminId: project.adminId,
 		});
 	} catch (error) {
 		console.log(error);
@@ -56,6 +57,7 @@ export const runMySqlBackup = async (mysql: MySql, backup: BackupSchedule) => {
 			type: "error",
 			// @ts-ignore
 			errorMessage: error?.message || "Error message not provided",
+			adminId: project.adminId,
 		});
 		throw error;
 	}
