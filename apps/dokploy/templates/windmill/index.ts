0799f8e04c75c654edab22368497e1c52fbda141
DrMxrcy <drmxrcy@gmail.com>
2024-11-12 13:05:56 -0500

    Fix: Windmill Path

--- apps/dokploy/templates/windmill/index.ts
+++ apps/dokploy/templates/windmill/index.ts
@@ -26,7 +26,7 @@ export function generate(schema: Schema): Template {
 
     const mounts: Template["mounts"] = [
         {
-            mountPath: "./Caddyfile",
+            filePath: "./Caddyfile",
             content: `{$BASE_URL} {
     bind {$ADDRESS}
     reverse_proxy /ws/* http://lsp:3001
