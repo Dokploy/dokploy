cab9443d25d70a8c888fae63cc19eee0a7e1d2d9
DrMxrcy <drmxrcy@gmail.com>
2024-11-13 04:44:13 -0500

    fix: Remove LinkStack
    Removed due to https://docs.linkstack.org/docker/reverse-proxies/#traefik

--- apps/dokploy/templates/templates.ts
+++ apps/dokploy/templates/templates.ts
@@ -787,20 +787,6 @@ export const templates: TemplateData[] = [
 		load: () => import("./slash/index").then((m) => m.generate),
 	},
 	{
-		id: "linkstack",
-		name: "LinkStack",
-		version: "latest",
-		description: "LinkStack is a highly customizable link sharing platform with an intuitive, easy to use user interface.",
-		logo: "linkstack.svg",
-		links: {
-			github: "https://github.com/LinkStackOrg/LinkStack",
-			website: "https://linkstack.org/",
-			docs: "https://docs.linkstack.org/",
-		},
-		tags: ["links", "sharing", "social"],
-			load: () => import("./linkstack/index").then((m) => m.generate),
-	},
-	{
 		id: "chatwoot",
 		name: "Chatwoot",
 		version: "v3.14.1",
