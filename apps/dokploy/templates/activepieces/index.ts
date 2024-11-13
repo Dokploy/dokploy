e57efa2e316551ae20a924cdb9eecc9a6f32012a
DrMxrcy <drmxrcy@gmail.com>
2024-11-13 02:47:30 -0500

    fix: AP Ports

--- apps/dokploy/templates/activepieces/index.ts
+++ apps/dokploy/templates/activepieces/index.ts
@@ -24,6 +24,8 @@ export function generate(schema: Schema): Template {
         `AP_POSTGRES_DATABASE=${postgresDb}`,
         `AP_POSTGRES_PASSWORD=${postgresPassword}`,
         `AP_POSTGRES_USERNAME=${postgresUser}`,
+        `AP_POSTGRES_PORT=5432`,
+        `AP_REDIS_PORT=6379`,
     ];
 
     return {
