763219e859dc2112343aedb778e535aacb5960c6
djknaeckebrot <dennis.lueke@pm.me>
2024-12-17 12:12:48 +0100

    refactor: streamline imports and improve code formatting in ShowSwarmNodes component

--- apps/dokploy/components/dashboard/swarm/show/show-nodes.tsx
+++ apps/dokploy/components/dashboard/swarm/show/show-nodes.tsx
@@ -1,16 +1,14 @@
-import React from "react";
-import { SwarmList, columns } from "./columns";
-import { DataTable } from "./data-table";
 import { api } from "@/utils/api";
+import React from "react";
+import { columns } from "./columns";
+import { DataTable } from "./data-table";
 
 function ShowSwarmNodes() {
-  const { data, isLoading } = api.swarm.getNodes.useQuery();
+	const { data, isLoading } = api.swarm.getNodes.useQuery();
 
-  console.log(data);
-
-  return (
-    <DataTable columns={columns} data={data ?? []} isLoading={isLoading} />
-  );
+	return (
+		<DataTable columns={columns} data={data ?? []} isLoading={isLoading} />
+	);
 }
 
 export default ShowSwarmNodes;
