f69fb7684b5b3aaaa77b3368b5c45f01c934c665
faytranevozter <mfahrurrifai@gmail.com>
2024-12-27 01:44:10 +0700

    fix: normalize state string to lowercase and update filter quotes in docker service functions

--- packages/server/src/services/docker.ts
+++ packages/server/src/services/docker.ts
@@ -196,7 +196,7 @@ export const getStackContainersByAppName = async (
 				: "No container name";
 
 			const state = parts[2]
-				? parts[2].replace("State: ", "").trim()
+				? parts[2].replace("State: ", "").trim().toLowerCase()
 				: "No state";
 			const node = parts[3]
 				? parts[3].replace("Node: ", "").trim()
@@ -255,7 +255,7 @@ export const getServiceContainersByAppName = async (
 				: "No container name";
 
 			const state = parts[2]
-				? parts[2].replace("State: ", "").trim()
+				? parts[2].replace("State: ", "").trim().toLowerCase()
 				: "No state";
 
 			const node = parts[3]
@@ -426,7 +426,7 @@ export const getNodeApplications = async (serverId?: string) => {
 			.trim()
 			.split("\n")
 			.map((line) => JSON.parse(line))
-			.filter((service) => !service.Name.startsWith('dokploy-'));
+			.filter((service) => !service.Name.startsWith("dokploy-"));
 
 		return appArray;
 	} catch (error) {}
