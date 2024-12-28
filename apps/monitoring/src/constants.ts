f88efc9127e6224022b3f268f5c9b83ab9ec6025
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2024-12-28 16:33:35 -0600

    refactor: update

--- apps/monitoring/src/constants.ts
+++ apps/monitoring/src/constants.ts
@@ -2,13 +2,13 @@ import path from "node:path";
 
 export const serverLogFile = path.join(
 	process.env.NODE_ENV === "production"
-		? "/etc/dokploy"
+		? "/etc"
 		: "/Users/mauricio/Documents/Github/Personal/dokploy/apps/dokploy/.docker",
 	"server_metrics.log",
 );
 export const containerLogFile = path.join(
 	process.env.NODE_ENV === "production"
-		? "/etc/dokploy"
+		? "/etc"
 		: "/Users/mauricio/Documents/Github/Personal/dokploy/apps/dokploy/.docker",
 	"containers_metrics.log",
 );
