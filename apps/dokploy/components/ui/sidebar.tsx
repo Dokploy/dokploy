175943f7abad69a2beb0d668a47dfd031559bf2c
190km <botmiley@gmail.com>
2025-01-13 20:28:31 +0100

    style: added custom-scrollbar to sidebar

--- apps/dokploy/components/ui/sidebar.tsx
+++ apps/dokploy/components/ui/sidebar.tsx
@@ -411,7 +411,7 @@ const SidebarContent = React.forwardRef<
 			ref={ref}
 			data-sidebar="content"
 			className={cn(
-				"flex min-h-0 flex-1 flex-col gap-2 overflow-auto group-data-[collapsible=icon]:overflow-hidden",
+				"flex min-h-0 flex-1 flex-col gap-2 custom-scrollbar overflow-auto group-data-[collapsible=icon]:overflow-hidden",
 				className,
 			)}
 			{...props}
