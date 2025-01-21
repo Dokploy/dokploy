1a44a0ea5a73ec0eaed6d13712ef4cffa4df8ca2
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-01-21 23:14:28 +1100

    refactor(template): use dokploy mount volume for superset_config.py

--- apps/dokploy/templates/superset/index.ts
+++ apps/dokploy/templates/superset/index.ts
@@ -31,8 +31,37 @@ export function generate(schema: Schema): Template {
 		`REDIS_PASSWORD=${redisPassword}`,
 	];
 
+	const mounts: Template["mounts"] = [
+		{
+			filePath: "./superset/superset_config.py",
+			content: `
+import os
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+MAPBOX_API_KEY = os.getenv("MAPBOX_API_KEY", "")
+
+CACHE_CONFIG = {
+  "CACHE_TYPE": "RedisCache",
+  "CACHE_DEFAULT_TIMEOUT": 300,
+  "CACHE_KEY_PREFIX": "superset_",
+  "CACHE_REDIS_HOST": "redis",
+  "CACHE_REDIS_PORT": 6379,
+  "CACHE_REDIS_DB": 1,
+  "CACHE_REDIS_URL": f"redis://:{os.getenv('REDIS_PASSWORD')}@redis:6379/1",
+}
+
+FILTER_STATE_CACHE_CONFIG = {**CACHE_CONFIG, "CACHE_KEY_PREFIX": "superset_filter_"}
+EXPLORE_FORM_DATA_CACHE_CONFIG = {**CACHE_CONFIG, "CACHE_KEY_PREFIX": "superset_explore_form_"}
+
+SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@db:5432/{os.getenv('POSTGRES_DB')}"
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+			`.trim(),
+		},
+	];
+
 	return {
 		envs,
 		domains,
+		mounts,
 	};
 }
