710f91272f7b67616c5fa156a5216e653cc09712
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-01-30 13:52:26 +1100

    fix(template): gotenberg use generatePassword

--- apps/dokploy/templates/gotenberg/index.ts
+++ apps/dokploy/templates/gotenberg/index.ts
@@ -1,13 +1,8 @@
-import {
-	type DomainSchema,
-	type Schema,
-	type Template,
-	generateRandomDomain,
-} from "../utils";
+import { type Schema, type Template, generatePassword } from "../utils";
 
 export function generate(schema: Schema): Template {
 	const username = "gotenberg";
-	const password = "changethis";
+	const password = generatePassword(32);
 
 	const envs = [
 		`GOTENBERG_API_BASIC_AUTH_USERNAME=${username}`,
