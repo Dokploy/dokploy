f7a29accb1d8335b200dca58cc6d911cc7158b16
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2025-01-30 23:40:01 -0600

    refactor: lint

--- apps/dokploy/templates/listmonk/index.ts
+++ apps/dokploy/templates/listmonk/index.ts
@@ -1,30 +1,30 @@
 import {
-  type DomainSchema,
-  type Schema,
-  type Template,
-  generateRandomDomain,
+	type DomainSchema,
+	type Schema,
+	type Template,
+	generateRandomDomain,
 } from "../utils";
 
 export function generate(schema: Schema): Template {
-  const randomDomain = generateRandomDomain(schema);
+	const randomDomain = generateRandomDomain(schema);
 
-  const domains: DomainSchema[] = [
-    {
-      host: randomDomain,
-      port: 9000,
-      serviceName: "app",
-    },
-  ];
+	const domains: DomainSchema[] = [
+		{
+			host: randomDomain,
+			port: 9000,
+			serviceName: "app",
+		},
+	];
 
-  const envs = [
-    "# visit the page to setup your super admin user",
-    "# check config.toml in Advanced / Volumes for more options",
-  ];
+	const envs = [
+		"# visit the page to setup your super admin user",
+		"# check config.toml in Advanced / Volumes for more options",
+	];
 
-  const mounts: Template["mounts"] = [
-    {
-      filePath: "config.toml",
-      content: `[app]
+	const mounts: Template["mounts"] = [
+		{
+			filePath: "config.toml",
+			content: `[app]
 address = "0.0.0.0:9000"
 
 [db]
@@ -41,12 +41,12 @@ max_lifetime = "300s"
 
 params = ""
 `,
-    },
-  ];
+		},
+	];
 
-  return {
-    envs,
-    mounts,
-    domains,
-  };
+	return {
+		envs,
+		mounts,
+		domains,
+	};
 }
