8b3e643ce76a4f7c0972404e8956b0cf0688394b
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2025-02-06 00:16:20 -0600

    Update packages/server/src/services/application.ts

--- packages/server/src/services/application.ts
+++ packages/server/src/services/application.ts
@@ -451,7 +451,7 @@ export const deployPreviewApplication = async ({
 			body: `### Dokploy Preview Deployment\n\n${buildingComment}`,
 		});
 		application.appName = previewDeployment.appName;
-		application.env = `${application.previewEnv}\nDOKPLOY_DEPLOY_URL=${previewDomain}`;
+		application.env = `${application.previewEnv}\nDOKPLOY_DEPLOY_URL=${previewDeployment?.domain?.host}`;
 		application.buildArgs = application.previewBuildArgs;
 
 		const admin = await findAdminById(application.project.adminId);
