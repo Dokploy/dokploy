baf555af5249e0d5d3e193e6304dc0fe870358f0
Cohvir <cohvirplayer@gmail.com>
2025-02-22 14:16:14 +0100

    feat(template): add Wiki.js

--- apps/dokploy/templates/templates.ts
+++ apps/dokploy/templates/templates.ts
@@ -1559,4 +1559,18 @@ export const templates: TemplateData[] = [
 		tags: ["backend", "database", "api"],
 		load: () => import("./convex/index").then((m) => m.generate),
 	},
+	{
+		id: "wikijs",
+		name: "Wiki.js",
+		version: "2.5",
+		description: "The most powerful and extensible open source Wiki software.",
+		logo: "wikijs.svg",
+		links: {
+			github: "https://github.com/requarks/wiki",
+			website: "https://js.wiki/",
+			docs: "https://docs.requarks.io/",
+		},
+		tags: ["knowledge-base", "self-hosted", "documentation"],
+		load: () => import("./wikijs/index").then((m) => m.generate),
+	},
 ];
