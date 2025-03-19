891dc840f5631e4288aab246e8c4a503ba55a94d
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2025-03-18 21:11:50 -0600

    feat(cluster): enhance node management UI with loading indicators and improved tab content
    - Added loading indicators in AddManager and AddWorker components to enhance user experience during data fetching.
    - Updated AddNode component to include overflow handling for tab content.
    - Renamed "Show Nodes" to "Show Swarm Nodes" in ShowNodesModal for clarity.

--- apps/dokploy/components/dashboard/settings/cluster/nodes/manager/add-manager.tsx
+++ apps/dokploy/components/dashboard/settings/cluster/nodes/manager/add-manager.tsx
@@ -6,7 +6,7 @@ import {
 } from "@/components/ui/dialog";
 import { api } from "@/utils/api";
 import copy from "copy-to-clipboard";
-import { CopyIcon } from "lucide-react";
+import { CopyIcon, Loader2 } from "lucide-react";
 import { toast } from "sonner";
 
 interface Props {
@@ -14,56 +14,63 @@ interface Props {
 }
 
 export const AddManager = ({ serverId }: Props) => {
-	const { data } = api.cluster.addManager.useQuery({ serverId });
+	const { data, isLoading } = api.cluster.addManager.useQuery({ serverId });
 
 	return (
 		<>
-			<div>
-				<CardContent className="sm:max-w-4xl max-h-screen overflow-y-auto flex flex-col gap-4 px-0">
-					<DialogHeader>
-						<DialogTitle>Add a new manager</DialogTitle>
-						<DialogDescription>Add a new manager</DialogDescription>
-					</DialogHeader>
-					<div className="flex flex-col gap-2.5 text-sm">
-						<span>1. Go to your new server and run the following command</span>
-						<span className="bg-muted rounded-lg p-2 flex justify-between">
-							curl https://get.docker.com | sh -s -- --version {data?.version}
-							<button
-								type="button"
-								className="self-center"
-								onClick={() => {
-									copy(
-										`curl https://get.docker.com | sh -s -- --version ${data?.version}`,
-									);
-									toast.success("Copied to clipboard");
-								}}
-							>
-								<CopyIcon className="h-4 w-4 cursor-pointer" />
-							</button>
-						</span>
-					</div>
+			<CardContent className="sm:max-w-4xl  flex flex-col gap-4 px-0">
+				<DialogHeader>
+					<DialogTitle>Add a new manager</DialogTitle>
+					<DialogDescription>Add a new manager</DialogDescription>
+				</DialogHeader>
+				{isLoading ? (
+					<Loader2 className="w-full animate-spin text-muted-foreground" />
+				) : (
+					<>
+						<div className="flex flex-col gap-2.5 text-sm">
+							<span>
+								1. Go to your new server and run the following command
+							</span>
+							<span className="bg-muted rounded-lg p-2 flex justify-between">
+								curl https://get.docker.com | sh -s -- --version {data?.version}
+								<button
+									type="button"
+									className="self-center"
+									onClick={() => {
+										copy(
+											`curl https://get.docker.com | sh -s -- --version ${data?.version}`,
+										);
+										toast.success("Copied to clipboard");
+									}}
+								>
+									<CopyIcon className="h-4 w-4 cursor-pointer" />
+								</button>
+							</span>
+						</div>
 
-					<div className="flex flex-col gap-2.5 text-sm">
-						<span>
-							2. Run the following command to add the node(manager) to your
-							cluster
-						</span>
-						<span className="bg-muted rounded-lg p-2  flex">
-							{data?.command}
-							<button
-								type="button"
-								className="self-start"
-								onClick={() => {
-									copy(data?.command || "");
-									toast.success("Copied to clipboard");
-								}}
-							>
-								<CopyIcon className="h-4 w-4 cursor-pointer" />
-							</button>
-						</span>
-					</div>
-				</CardContent>
-			</div>
+						<div className="flex flex-col gap-2.5 text-sm">
+							<span>
+								2. Run the following command to add the node(manager) to your
+								cluster
+							</span>
+
+							<span className="bg-muted rounded-lg p-2  flex">
+								{data?.command}
+								<button
+									type="button"
+									className="self-start"
+									onClick={() => {
+										copy(data?.command || "");
+										toast.success("Copied to clipboard");
+									}}
+								>
+									<CopyIcon className="h-4 w-4 cursor-pointer" />
+								</button>
+							</span>
+						</div>
+					</>
+				)}
+			</CardContent>
 		</>
 	);
 };
--- apps/dokploy/components/dashboard/settings/cluster/nodes/workers/add-worker.tsx
+++ apps/dokploy/components/dashboard/settings/cluster/nodes/workers/add-worker.tsx
@@ -6,7 +6,7 @@ import {
 } from "@/components/ui/dialog";
 import { api } from "@/utils/api";
 import copy from "copy-to-clipboard";
-import { CopyIcon } from "lucide-react";
+import { CopyIcon, Loader2 } from "lucide-react";
 import { toast } from "sonner";
 
 interface Props {
@@ -14,54 +14,59 @@ interface Props {
 }
 
 export const AddWorker = ({ serverId }: Props) => {
-	const { data } = api.cluster.addWorker.useQuery({ serverId });
+	const { data, isLoading } = api.cluster.addWorker.useQuery({ serverId });
 
 	return (
-		<div>
-			<CardContent className="sm:max-w-4xl max-h-screen overflow-y-auto flex flex-col gap-4 px-0">
-				<DialogHeader>
-					<DialogTitle>Add a new worker</DialogTitle>
-					<DialogDescription>Add a new worker</DialogDescription>
-				</DialogHeader>
-				<div className="flex flex-col gap-2.5 text-sm">
-					<span>1. Go to your new server and run the following command</span>
-					<span className="bg-muted rounded-lg p-2 flex justify-between">
-						curl https://get.docker.com | sh -s -- --version {data?.version}
-						<button
-							type="button"
-							className="self-center"
-							onClick={() => {
-								copy(
-									`curl https://get.docker.com | sh -s -- --version ${data?.version}`,
-								);
-								toast.success("Copied to clipboard");
-							}}
-						>
-							<CopyIcon className="h-4 w-4 cursor-pointer" />
-						</button>
-					</span>
-				</div>
+		<CardContent className="sm:max-w-4xl   flex flex-col gap-4 px-0">
+			<DialogHeader>
+				<DialogTitle>Add a new worker</DialogTitle>
+				<DialogDescription>Add a new worker</DialogDescription>
+			</DialogHeader>
+			{isLoading ? (
+				<Loader2 className="w-full animate-spin text-muted-foreground" />
+			) : (
+				<>
+					<div className="flex flex-col gap-2.5 text-sm">
+						<span>1. Go to your new server and run the following command</span>
+						<span className="bg-muted rounded-lg p-2 flex justify-between">
+							curl https://get.docker.com | sh -s -- --version {data?.version}
+							<button
+								type="button"
+								className="self-center"
+								onClick={() => {
+									copy(
+										`curl https://get.docker.com | sh -s -- --version ${data?.version}`,
+									);
+									toast.success("Copied to clipboard");
+								}}
+							>
+								<CopyIcon className="h-4 w-4 cursor-pointer" />
+							</button>
+						</span>
+					</div>
 
-				<div className="flex flex-col gap-2.5 text-sm">
-					<span>
-						2. Run the following command to add the node(worker) to your cluster
-					</span>
+					<div className="flex flex-col gap-2.5 text-sm">
+						<span>
+							2. Run the following command to add the node(worker) to your
+							cluster
+						</span>
 
-					<span className="bg-muted rounded-lg p-2  flex">
-						{data?.command}
-						<button
-							type="button"
-							className="self-start"
-							onClick={() => {
-								copy(data?.command || "");
-								toast.success("Copied to clipboard");
-							}}
-						>
-							<CopyIcon className="h-4 w-4 cursor-pointer" />
-						</button>
-					</span>
-				</div>
-			</CardContent>
-		</div>
+						<span className="bg-muted rounded-lg p-2  flex">
+							{data?.command}
+							<button
+								type="button"
+								className="self-start"
+								onClick={() => {
+									copy(data?.command || "");
+									toast.success("Copied to clipboard");
+								}}
+							>
+								<CopyIcon className="h-4 w-4 cursor-pointer" />
+							</button>
+						</span>
+					</div>
+				</>
+			)}
+		</CardContent>
 	);
 };
