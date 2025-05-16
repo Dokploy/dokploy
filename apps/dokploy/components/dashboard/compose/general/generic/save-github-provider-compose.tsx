c2c29dbaba3589a7cdeaabb8f25e663e5a5757f6
yergom <albertyeragomez@gmail.com>
2025-05-16 13:25:28 +0000

    fix: more informative placeholder for watch path

--- apps/dokploy/components/dashboard/compose/general/generic/save-github-provider-compose.tsx
+++ apps/dokploy/components/dashboard/compose/general/generic/save-github-provider-compose.tsx
@@ -474,7 +474,7 @@ export const SaveGithubProviderCompose = ({ composeId }: Props) => {
 										<FormControl>
 											<div className="flex gap-2">
 												<Input
-													placeholder="Enter a path to watch (e.g., src/*, dist/*)"
+													placeholder="Enter a path to watch (e.g., src/**, dist/*.js)"
 													onKeyDown={(e) => {
 														if (e.key === "Enter") {
 															e.preventDefault();
@@ -496,7 +496,7 @@ export const SaveGithubProviderCompose = ({ composeId }: Props) => {
 													variant="secondary"
 													onClick={() => {
 														const input = document.querySelector(
-															'input[placeholder="Enter a path to watch (e.g., src/*, dist/*)"]',
+															'input[placeholder="Enter a path to watch (e.g., src/**, dist/*.js)"]',
 														) as HTMLInputElement;
 														const value = input.value.trim();
 														if (value) {
