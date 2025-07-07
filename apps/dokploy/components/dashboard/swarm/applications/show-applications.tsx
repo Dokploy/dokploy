14257a3d15780b00cbb014caafabe05f71c23d14
Noé Grangier <noe@lonestone.io>
2025-07-07 16:59:47 +0200

    refacto(translations): Added internationalization for swarm

--- apps/dokploy/components/dashboard/swarm/applications/show-applications.tsx
+++ apps/dokploy/components/dashboard/swarm/applications/show-applications.tsx
@@ -9,6 +9,7 @@ import {
 } from "@/components/ui/dialog";
 import { api } from "@/utils/api";
 import { Layers, Loader2 } from "lucide-react";
+import { useTranslation } from "next-i18next";
 import { type ApplicationList, columns } from "./columns";
 import { DataTable } from "./data-table";
 
@@ -17,6 +18,7 @@ interface Props {
 }
 
 export const ShowNodeApplications = ({ serverId }: Props) => {
+	const { t } = useTranslation("dashboard");
 	const { data: NodeApps, isLoading: NodeAppsLoading } =
 		api.swarm.getNodeApps.useQuery({ serverId });
 
@@ -44,7 +46,7 @@ export const ShowNodeApplications = ({ serverId }: Props) => {
 	if (!NodeApps || !NodeAppDetails) {
 		return (
 			<span className="text-sm w-full flex text-center justify-center items-center">
-				No data found
+				{t("dashboard.swarm.noDataFound")}
 			</span>
 		);
 	}
@@ -84,14 +86,14 @@ export const ShowNodeApplications = ({ serverId }: Props) => {
 			<DialogTrigger asChild>
 				<Button variant="outline" size="sm" className="w-full">
 					<Layers className="h-4 w-4 mr-2" />
-					Services
+					{t("dashboard.swarm.services")}
 				</Button>
 			</DialogTrigger>
 			<DialogContent className={"sm:max-w-10xl overflow-y-auto max-h-screen"}>
 				<DialogHeader>
-					<DialogTitle>Node Applications</DialogTitle>
+					<DialogTitle>{t("dashboard.swarm.nodeApplications")}</DialogTitle>
 					<DialogDescription>
-						See in detail the applications running on this node
+						{t("dashboard.swarm.nodeApplicationsDescription")}
 					</DialogDescription>
 				</DialogHeader>
 				<div className="max-h-[80vh]">
