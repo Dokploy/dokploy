1f205470734d1d043c52897b4422587be1bac0e3
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2025-07-28 19:46:34 +0900

    feat: bump traefik to v3.2.2

--- apps/dokploy/setup.ts
+++ apps/dokploy/setup.ts
@@ -21,7 +21,7 @@ import {
 		await initializeNetwork();
 		createDefaultTraefikConfig();
 		createDefaultServerTraefikConfig();
-		await execAsync("docker pull traefik:v3.1.2");
+		await execAsync("docker pull traefik:v3.2.2");
 		await initializeTraefik();
 		await initializeRedis();
 		await initializePostgres();
