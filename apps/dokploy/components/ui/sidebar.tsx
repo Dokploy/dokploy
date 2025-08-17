6d9a1db8af022b31fc68d97a06a391148c2399f3
Vyacheslav Scherbinin <gentslava@mail.ru>
2025-08-17 12:36:40 +0700

    fix(ui): scrollbar gutter stable

--- apps/dokploy/components/ui/sidebar.tsx
+++ apps/dokploy/components/ui/sidebar.tsx
@@ -329,10 +329,11 @@ const SidebarInset = React.forwardRef<
 		<main
 			ref={ref}
 			className={cn(
-				"relative flex min-h-svh  overflow-auto w-full flex-col bg-background",
+				"relative flex min-h-svh overflow-auto w-full flex-col bg-background",
 				"peer-data-[variant=inset]:min-h-[calc(100svh-theme(spacing.4))] md:peer-data-[variant=inset]:m-2 md:peer-data-[state=collapsed]:peer-data-[variant=inset]:ml-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow",
 				className,
 			)}
+			style={{ scrollbarGutter: "stable" }}
 			{...props}
 		/>
 	);
