e3979d2c481cb87293b5bcca314a988365685b88
Vyacheslav Scherbinin <gentslava@mail.ru>
2025-08-17 12:09:26 +0700

    fix(ui): fixed viewport

--- apps/dokploy/components/ui/sidebar.tsx
+++ apps/dokploy/components/ui/sidebar.tsx
@@ -148,7 +148,7 @@ const SidebarProvider = React.forwardRef<
 							} as React.CSSProperties
 						}
 						className={cn(
-							"group/sidebar-wrapper flex min-h-svh w-full has-[[data-variant=inset]]:bg-sidebar",
+							"group/sidebar-wrapper flex h-svh w-full has-[[data-variant=inset]]:bg-sidebar",
 							className,
 						)}
 						ref={ref}
