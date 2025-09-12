685cee5e2b11d1042c07ac598bc36ee8bb49f0a6
Charlie Fox <charlie@polarise.io>
2025-09-12 22:03:09 +0100

    feat(api): Added api outputs for databases

---
+++ packages/server/src/api/schemas/mariadb.ts
@@ -0,0 +1,126 @@
+import { backups } from "@dokploy/server/db/schema/backups";
+import { environments } from "@dokploy/server/db/schema/environment";
+import { mariadb } from "@dokploy/server/db/schema/mariadb";
+import { mounts } from "@dokploy/server/db/schema/mount";
+import { projects } from "@dokploy/server/db/schema/project";
+import { server } from "@dokploy/server/db/schema/server";
+import {
+	HealthCheckSwarmSchema,
+	LabelsSwarmSchema,
+	NetworkSwarmSchema,
+	PlacementSwarmSchema,
+	RestartPolicySwarmSchema,
+	ServiceModeSwarmSchema,
+	UpdateConfigSwarmSchema,
+} from "@dokploy/server/db/schema/shared";
+import { createSelectSchema } from "drizzle-zod";
+import { z } from "zod";
+
+const mariadbSelectSchema = createSelectSchema(mariadb).extend({
+	healthCheckSwarm: HealthCheckSwarmSchema.nullable(),
+	restartPolicySwarm: RestartPolicySwarmSchema.nullable(),
+	placementSwarm: PlacementSwarmSchema.nullable(),
+	updateConfigSwarm: UpdateConfigSwarmSchema.nullable(),
+	rollbackConfigSwarm: UpdateConfigSwarmSchema.nullable(),
+	modeSwarm: ServiceModeSwarmSchema.nullable(),
+	labelsSwarm: LabelsSwarmSchema.nullable(),
+	networkSwarm: NetworkSwarmSchema.nullable(),
+});
+const environmentSelectSchema = createSelectSchema(environments).extend({
+	project: createSelectSchema(projects),
+});
+const mountSelectSchema = createSelectSchema(mounts);
+const serverSelectSchema = createSelectSchema(server);
+const backupSelectSchema = createSelectSchema(backups).extend({
+	metadata: z
+		.union([
+			z.object({
+				postgres: z
+					.object({
+						databaseUser: z.string(),
+					})
+					.optional(),
+				mariadb: z
+					.object({
+						databaseUser: z.string(),
+						databasePassword: z.string(),
+					})
+					.optional(),
+				mongo: z
+					.object({
+						databaseUser: z.string(),
+						databasePassword: z.string(),
+					})
+					.optional(),
+				mysql: z
+					.object({
+						databaseRootPassword: z.string(),
+					})
+					.optional(),
+			}),
+			z.null(),
+		])
+		.optional(),
+});
+
+export const apiCreateMariaDB = mariadbSelectSchema.omit({ mariadbId: true });
+
+export const apiCreateMariaDBOutput = mariadbSelectSchema;
+
+export const apiFindOneMariaDB = mariadbSelectSchema.pick({ mariadbId: true });
+
+export const apiFindOneMariaDBOutput = mariadbSelectSchema.extend({
+	environment: environmentSelectSchema,
+	mounts: z.array(mountSelectSchema),
+	server: serverSelectSchema.nullable(),
+	backups: z.array(backupSelectSchema),
+});
+
+export const apiUpdateMariaDB = mariadbSelectSchema.partial();
+
+export const apiDeleteMariaDB = mariadbSelectSchema.pick({ mariadbId: true });
+
+export const apiStartMariaDBOutput = mariadbSelectSchema.extend({
+	environment: environmentSelectSchema,
+	mounts: z.array(mountSelectSchema),
+	server: serverSelectSchema.nullable(),
+	backups: z.array(backupSelectSchema),
+});
+
+export const apiStopMariaDBOutput = mariadbSelectSchema.extend({
+	environment: environmentSelectSchema,
+	mounts: z.array(mountSelectSchema),
+	server: serverSelectSchema.nullable(),
+	backups: z.array(backupSelectSchema),
+});
+
+export const apiSaveExternalPortMariaDBOutput = mariadbSelectSchema.extend({
+	environment: environmentSelectSchema,
+	mounts: z.array(mountSelectSchema),
+	server: serverSelectSchema.nullable(),
+	backups: z.array(backupSelectSchema),
+});
+
+export const apiDeployMariaDBOutput = mariadbSelectSchema.extend({
+	environment: environmentSelectSchema,
+	mounts: z.array(mountSelectSchema),
+	server: serverSelectSchema.nullable(),
+	backups: z.array(backupSelectSchema),
+});
+
+export const apiChangeMariaDBStatusOutput = mariadbSelectSchema.extend({
+	environment: environmentSelectSchema,
+	mounts: z.array(mountSelectSchema),
+	server: serverSelectSchema.nullable(),
+	backups: z.array(backupSelectSchema),
+});
+
+export const apiRemoveMariaDBOutput = mariadbSelectSchema.extend({
+	environment: environmentSelectSchema,
+	mounts: z.array(mountSelectSchema),
+	server: serverSelectSchema.nullable(),
+	backups: z.array(backupSelectSchema),
+});
+
+export const apiMoveMariaDBOutput = mariadbSelectSchema;
+
---
+++ packages/server/src/api/schemas/redis.ts
@@ -0,0 +1,86 @@
+import { environments } from "@dokploy/server/db/schema/environment";
+import { mounts } from "@dokploy/server/db/schema/mount";
+import { projects } from "@dokploy/server/db/schema/project";
+import { redis } from "@dokploy/server/db/schema/redis";
+import { server } from "@dokploy/server/db/schema/server";
+import {
+	HealthCheckSwarmSchema,
+	LabelsSwarmSchema,
+	NetworkSwarmSchema,
+	PlacementSwarmSchema,
+	RestartPolicySwarmSchema,
+	ServiceModeSwarmSchema,
+	UpdateConfigSwarmSchema,
+} from "@dokploy/server/db/schema/shared";
+import { createSelectSchema } from "drizzle-zod";
+import { z } from "zod";
+
+const redisSelectSchema = createSelectSchema(redis).extend({
+	healthCheckSwarm: HealthCheckSwarmSchema.nullable(),
+	restartPolicySwarm: RestartPolicySwarmSchema.nullable(),
+	placementSwarm: PlacementSwarmSchema.nullable(),
+	updateConfigSwarm: UpdateConfigSwarmSchema.nullable(),
+	rollbackConfigSwarm: UpdateConfigSwarmSchema.nullable(),
+	modeSwarm: ServiceModeSwarmSchema.nullable(),
+	labelsSwarm: LabelsSwarmSchema.nullable(),
+	networkSwarm: NetworkSwarmSchema.nullable(),
+});
+const environmentSelectSchema = createSelectSchema(environments).extend({
+	project: createSelectSchema(projects),
+});
+const mountSelectSchema = createSelectSchema(mounts);
+const serverSelectSchema = createSelectSchema(server);
+
+export const apiCreateRedis = redisSelectSchema.omit({ redisId: true });
+
+export const apiCreateRedisOutput = redisSelectSchema;
+
+export const apiFindOneRedis = redisSelectSchema.pick({ redisId: true });
+
+export const apiFindOneRedisOutput = redisSelectSchema.extend({
+	environment: environmentSelectSchema,
+	mounts: z.array(mountSelectSchema),
+	server: serverSelectSchema.nullable(),
+});
+
+export const apiUpdateRedis = redisSelectSchema.partial();
+
+export const apiDeleteRedis = redisSelectSchema.pick({ redisId: true });
+
+export const apiStartRedisOutput = redisSelectSchema.extend({
+	environment: environmentSelectSchema,
+	mounts: z.array(mountSelectSchema),
+	server: serverSelectSchema.nullable(),
+});
+
+export const apiStopRedisOutput = redisSelectSchema.extend({
+	environment: environmentSelectSchema,
+	mounts: z.array(mountSelectSchema),
+	server: serverSelectSchema.nullable(),
+});
+
+export const apiSaveExternalPortRedisOutput = redisSelectSchema.extend({
+	environment: environmentSelectSchema,
+	mounts: z.array(mountSelectSchema),
+	server: serverSelectSchema.nullable(),
+});
+
+export const apiDeployRedisOutput = redisSelectSchema.extend({
+	environment: environmentSelectSchema,
+	mounts: z.array(mountSelectSchema),
+	server: serverSelectSchema.nullable(),
+});
+
+export const apiChangeRedisStatusOutput = redisSelectSchema.extend({
+	environment: environmentSelectSchema,
+	mounts: z.array(mountSelectSchema),
+	server: serverSelectSchema.nullable(),
+});
+
+export const apiRemoveRedisOutput = redisSelectSchema.extend({
+	environment: environmentSelectSchema,
+	mounts: z.array(mountSelectSchema),
+	server: serverSelectSchema.nullable(),
+});
+
+export const apiMoveRedisOutput = redisSelectSchema;
