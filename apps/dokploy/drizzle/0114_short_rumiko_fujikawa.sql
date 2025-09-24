4bced9ede0dca9f60a14d0c2338a5f744ad95eee
Lucas Manchine <lucasperinm@gmail.com>
2025-09-24 12:00:09 -0300

    fix: db migrations for stop grace period swarm

---
+++ apps/dokploy/drizzle/0114_short_rumiko_fujikawa.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "mariadb" ADD COLUMN "stopGracePeriodSwarm" bigint;--> statement-breakpoint
+ALTER TABLE "mongo" ADD COLUMN "stopGracePeriodSwarm" bigint;--> statement-breakpoint
+ALTER TABLE "mysql" ADD COLUMN "stopGracePeriodSwarm" bigint;--> statement-breakpoint
+ALTER TABLE "postgres" ADD COLUMN "stopGracePeriodSwarm" bigint;--> statement-breakpoint
+ALTER TABLE "redis" ADD COLUMN "stopGracePeriodSwarm" bigint;
