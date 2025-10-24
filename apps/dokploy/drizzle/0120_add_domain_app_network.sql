d6cd593e63287bbbc1f7bada50ec292462747388
Antoine <antoinechiris@gmail.com>
2025-10-24 16:10:11 +0200

    fix: migration file

--- apps/dokploy/drizzle/0120_add_domain_app_network.sql
+++ apps/dokploy/drizzle/0120_add_domain_app_network.sql
@@ -1,2 +1,3 @@
+ALTER TABLE "application" ADD COLUMN "previewNetworkIds" text[];--> statement-breakpoint
 ALTER TABLE "domain" ADD COLUMN "networkId" text;--> statement-breakpoint
 ALTER TABLE "domain" ADD CONSTRAINT "domain_networkId_network_networkId_fk" FOREIGN KEY ("networkId") REFERENCES "public"."network"("networkId") ON DELETE set null ON UPDATE no action;
