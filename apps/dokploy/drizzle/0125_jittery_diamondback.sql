5d5af8f57f221f51ec661075160a27086af41cd6
Mauricio Siu <siumauricio@hotmail.com>
2025-12-07 13:16:22 -0600

    chore: remove obsolete SQL files and journal entries for custom notification type
    - Deleted SQL file related to the 'custom' notification type and its schema changes.
    - Removed corresponding journal entry from the metadata to reflect the deletion of the custom notification type.

--- apps/dokploy/drizzle/0125_jittery_diamondback.sql
+++
@@ -1,9 +0,0 @@
-ALTER TYPE "public"."notificationType" ADD VALUE 'custom' BEFORE 'lark';--> statement-breakpoint
-CREATE TABLE "custom" (
-	"customId" text PRIMARY KEY NOT NULL,
-	"endpoint" text NOT NULL,
-	"headers" text
-);
---> statement-breakpoint
-ALTER TABLE "notification" ADD COLUMN "customId" text;--> statement-breakpoint
-ALTER TABLE "notification" ADD CONSTRAINT "notification_customId_custom_customId_fk" FOREIGN KEY ("customId") REFERENCES "public"."custom"("customId") ON DELETE cascade ON UPDATE no action;
