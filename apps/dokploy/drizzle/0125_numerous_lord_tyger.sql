a3fa25fadca8aaeb236e7de4b4cf55c98db9c521
Bima42 <tanpau42@gmail.com>
2025-12-14 10:40:01 +0100

    chore: clear old migration

--- apps/dokploy/drizzle/0125_numerous_lord_tyger.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "organization" ADD COLUMN "lastNotifiedUpdateVersion" text;--> statement-breakpoint
-ALTER TABLE "notification" ADD COLUMN "dokployUpdate" boolean DEFAULT false NOT NULL;
