7430c33586893dd8cc615776245def61d6a66110
sugu <sugugalga@seabed2crest.com>
2026-02-16 16:10:08 +0530

    fix: revert postgres port to 5432

--- packages/server/src/setup/postgres-setup.ts
+++ packages/server/src/setup/postgres-setup.ts
@@ -37,7 +37,7 @@ export const initializePostgres = async () => {
 				Ports: [
 					{
 						TargetPort: 5432,
-						PublishedPort: 5433,
+						PublishedPort: 5432,
 						Protocol: "tcp",
 						PublishMode: "host",
 					},
