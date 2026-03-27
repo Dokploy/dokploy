15595ad88f81e3f0c210bf32a51bd536515d37a9
mk <kamai122000@mail.ru>
2026-03-27 16:56:20 +0300

    update config

--- apps/dokploy/docker/feat.sh
+++ apps/dokploy/docker/feat.sh
@@ -3,8 +3,8 @@
 
 # BUILDER=$(docker buildx create --use)
 
-# docker buildx build --platform linux/amd64,linux/arm64 --pull --rm -t "dokploy/dokploy:feature" -f 'Dockerfile' --push .
+# docker buildx build --platform linux/amd64,linux/arm64 --pull --rm -t "kamik2k/deploybox:feature" -f 'Dockerfile' --push .
 
-docker build --platform linux/amd64 --pull --rm -t "dokploy/dokploy:feature" -f 'Dockerfile' .
+docker build --platform linux/amd64 --pull --rm -t "kamik2k/deploybox:feature" -f 'Dockerfile' .
 
-# docker  build --platform linux/amd64 --pull --rm -t "dokploy/dokploy:feature" -f 'Dockerfile' .
+# docker  build --platform linux/amd64 --pull --rm -t "kamik2k/deploybox:feature" -f 'Dockerfile' .
--- apps/dokploy/docker/push.sh
+++ apps/dokploy/docker/push.sh
@@ -8,11 +8,11 @@ BUILDER=$(docker buildx create --use)
 if [ "$BUILD_TYPE" == "canary" ]; then
     TAG="canary"
     echo PUSHING CANARY
-        docker buildx build --platform linux/amd64,linux/arm64 --pull --rm -t "dokploy/dokploy:${TAG}" -f 'Dockerfile' --push .
+        docker buildx build --platform linux/amd64,linux/arm64 --pull --rm -t "kamik2k/deploybox:${TAG}" -f 'Dockerfile' --push .
 else
     echo  "PUSHING PRODUCTION"
     VERSION=$(node -p "require('./package.json').version")
-    docker buildx build --platform linux/amd64,linux/arm64 --pull --rm -t "dokploy/dokploy:latest" -t "dokploy/dokploy:${VERSION}" -f 'Dockerfile' --push .
+    docker buildx build --platform linux/amd64,linux/arm64 --pull --rm -t "kamik2k/deploybox:latest" -t "kamik2k/deploybox:${VERSION}" -f 'Dockerfile' --push .
 fi
 
 docker buildx rm $BUILDER
