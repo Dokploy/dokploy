dca7d51779d3a0af09447b9f7102054ab3b71fb9
Oro <boyer63nicolas@gmail.com>
2026-03-28 14:44:31 +0100

    fix: add additional breakpoints

--- apps/dokploy/components/ui/cards-layout.tsx
+++ apps/dokploy/components/ui/cards-layout.tsx
@@ -98,7 +98,7 @@ const CardsLayout = React.forwardRef<HTMLDivElement, CardsLayoutProps>(
 				<div
 					className={cn(
 						layout === Layout.GRID
-							? "gap-5 pb-10 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3"
+							? "gap-5 pb-10 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 3xl:grid-cols-5"
 							: "flex flex-col gap-4 w-full",
 						className,
 					)}
