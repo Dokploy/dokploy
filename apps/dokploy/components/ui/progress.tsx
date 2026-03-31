0735301bb661c2a468d67db6ce12797caea385cb
Jeremy <jjmileyjr@gmail.com>
2026-03-31 07:43:51 +0200

    restyle checkbox, switch, radio, popover, dropdown, progress, sidebar

--- apps/dokploy/components/ui/progress.tsx
+++ apps/dokploy/components/ui/progress.tsx
@@ -10,13 +10,13 @@ const Progress = React.forwardRef<
 	<ProgressPrimitive.Root
 		ref={ref}
 		className={cn(
-			"relative h-4 w-full overflow-hidden rounded-full bg-secondary",
+			"relative h-2 w-full overflow-hidden rounded-full bg-secondary",
 			className,
 		)}
 		{...props}
 	>
 		<ProgressPrimitive.Indicator
-			className="h-full w-full flex-1 bg-primary transition-all"
+			className="h-full w-full flex-1 bg-primary rounded-full transition-all"
 			style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
 		/>
 	</ProgressPrimitive.Root>
