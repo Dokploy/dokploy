4c36a75ce9cfba96538daf9e04449d4d0e0ac3b8
Jeremy <jjmileyjr@gmail.com>
2026-03-31 07:43:51 +0200

    restyle button, input, and card components

--- apps/dokploy/components/ui/card.tsx
+++ apps/dokploy/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
 	<div
 		ref={ref}
 		className={cn(
-			"rounded-lg border bg-card text-card-foreground shadow-sm",
+			"rounded-lg border bg-card text-card-foreground",
 			className,
 		)}
 		{...props}
@@ -23,7 +23,7 @@ const CardHeader = React.forwardRef<
 >(({ className, ...props }, ref) => (
 	<div
 		ref={ref}
-		className={cn("flex flex-col space-y-1.5 p-6", className)}
+		className={cn("flex flex-col gap-1.5 p-6", className)}
 		{...props}
 	/>
 ));
@@ -36,7 +36,7 @@ const CardTitle = React.forwardRef<
 	<h3
 		ref={ref}
 		className={cn(
-			"text-2xl font-semibold leading-none tracking-tight",
+			"text-base font-medium leading-none tracking-tight",
 			className,
 		)}
 		{...props}
