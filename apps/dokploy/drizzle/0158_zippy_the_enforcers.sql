781cf95fd728be0310cc79cf500618a5649dd11e
EphraimHaber <ephraimxyz@gmail.com>
2026-04-04 05:45:37 +0300

    removed for clean rebase

--- apps/dokploy/drizzle/0158_zippy_the_enforcers.sql
+++
@@ -1,5 +0,0 @@
-ALTER TYPE "public"."RegistryType" ADD VALUE 'awsEcr';--> statement-breakpoint
-ALTER TABLE "domain" ADD COLUMN "customEntrypoint" text;--> statement-breakpoint
-ALTER TABLE "registry" ADD COLUMN "awsAccessKeyId" text;--> statement-breakpoint
-ALTER TABLE "registry" ADD COLUMN "awsSecretAccessKey" text;--> statement-breakpoint
-ALTER TABLE "registry" ADD COLUMN "awsRegion" text;
