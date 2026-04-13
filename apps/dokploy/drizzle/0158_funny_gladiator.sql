f3988baa0909402fb3809096dcf66787980dbaf5
Bima42 <tanpau42@gmail.com>
2026-04-13 08:49:01 +0200

    chore: clear migration

--- apps/dokploy/drizzle/0158_funny_gladiator.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "organization" ADD COLUMN "lastNotifiedUpdateVersion" text;--> statement-breakpoint
-ALTER TABLE "domain" ADD COLUMN "customEntrypoint" text;--> statement-breakpoint
-ALTER TABLE "notification" ADD COLUMN "dokployUpdate" boolean DEFAULT false NOT NULL;
