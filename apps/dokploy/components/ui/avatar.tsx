2ddaa0dd907eaf4ff2470fdfdf02d9d15f4232e5
ItzzMateo <itzzmateo@devflare.de>
2026-08-15 13:09:24 +0200

    fix: clip avatar images to rounded border

--- apps/dokploy/components/ui/avatar.tsx
+++ apps/dokploy/components/ui/avatar.tsx
@@ -17,7 +17,7 @@ function Avatar({
 			data-slot="avatar"
 			data-size={size}
 			className={cn(
-				"group/avatar relative flex size-10 shrink-0 rounded-full select-none after:absolute after:inset-0 after:rounded-full after:border after:border-border after:mix-blend-darken data-[size=lg]:size-10 data-[size=sm]:size-6 dark:after:mix-blend-lighten",
+				"group/avatar relative flex size-10 shrink-0 overflow-hidden rounded-full select-none after:absolute after:inset-0 after:rounded-[inherit] after:border after:border-border after:mix-blend-darken data-[size=lg]:size-10 data-[size=sm]:size-6 dark:after:mix-blend-lighten",
 				className,
 			)}
 			{...props}
