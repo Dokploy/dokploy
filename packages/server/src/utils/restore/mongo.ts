8dd3c1e8cff59f2f6a6d7c5b9ae0d02935b1080e
Furox <real.furkannn@gmail.com>
2026-09-04 20:27:06 +0300

    feat: restore mongo from generic destinations

--- packages/server/src/utils/restore/mongo.ts
+++ packages/server/src/utils/restore/mongo.ts
@@ -3,7 +3,7 @@ import type { Destination } from "@dokploy/server/services/destination";
 import type { Mongo } from "@dokploy/server/services/mongo";
 import { quote } from "shell-quote";
 import type { z } from "zod";
-import { getS3Credentials } from "../backups/utils";
+import { getRclonePathAndFlags } from "../backups/utils";
 import { execAsync, execAsyncRemote } from "../process/execAsync";
 import { getRestoreCommand } from "./utils";
 
@@ -16,9 +16,8 @@ export const restoreMongoBackup = async (
 	try {
 		const { appName, databasePassword, databaseUser, serverId } = mongo;
 
-		const rcloneFlags = getS3Credentials(destination);
-		const bucketPath = `:s3:${destination.bucket}`;
-		const backupPath = `${bucketPath}/${backupInput.backupFile}`;
+		const { flags: rcloneFlags, path: backupPath } =
+			await getRclonePathAndFlags(destination, backupInput.backupFile);
 		const rcloneCommand = `rclone copy ${rcloneFlags.join(" ")} ${quote([backupPath])}`;
 
 		const command = getRestoreCommand({
