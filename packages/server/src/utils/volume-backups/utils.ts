383bb7ea9fca436cae9f87d0b232be2ac8c4ddbe
tokenjunkielabs <tokenjunkielabs@gmail.com>
2026-09-20 18:50:07 -0400

    feat(backups): retain volume backups on generic rclone destinations

--- packages/server/src/utils/volume-backups/utils.ts
+++ packages/server/src/utils/volume-backups/utils.ts
@@ -11,7 +11,11 @@ import {
 	execAsyncRemote,
 } from "@dokploy/server/utils/process/execAsync";
 import { scheduledJobs, scheduleJob } from "node-schedule";
-import { getS3Credentials, normalizeS3Path } from "../backups/utils";
+import {
+	getRcloneDestination,
+	joinRclonePath,
+} from "../backups/rclone-destination";
+import { normalizeS3Path } from "../backups/utils";
 import { sendVolumeBackupNotifications } from "../notifications/volume-backup";
 import { backupVolume, getVolumeServiceAppName } from "./backup";
 
@@ -84,9 +88,12 @@ const cleanupOldVolumeBackups = async (
 	if (!keepLatestCount) return;
 
 	try {
-		const rcloneFlags = getS3Credentials(destination);
+		const { flags: rcloneFlags, remoteRoot } = getRcloneDestination(destination);
 		const s3AppName = getVolumeServiceAppName(volumeBackup);
-		const backupFilesPath = `:s3:${destination.bucket}/${s3AppName}/${normalizeS3Path(prefix || "")}`;
+		const backupFilesPath = joinRclonePath(
+			remoteRoot,
+			`${s3AppName}/${normalizeS3Path(prefix || "")}`,
+		);
 		const listCommand = `rclone lsf ${rcloneFlags.join(" ")} --include \"${volumeName}-*.tar\" ${backupFilesPath}`;
 		const sortAndPick = `sort -r | tail -n +$((${keepLatestCount}+1)) | xargs -I{}`;
 		const deleteCommand = `rclone delete ${rcloneFlags.join(" ")} ${backupFilesPath}{}`;
